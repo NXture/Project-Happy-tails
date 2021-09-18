b8cdd2686dc17d60010b52d77939379a5febbc10
NXture <varunvaru27101999@gmail.com>
2021-09-18 12:13:09 +0530

    fixed info error

--- pages/index.js
+++ pages/index.js
@@ -66,13 +66,13 @@ export default function Home(props) {
         <h1>🐶 Hi!</h1>
         <p>Welcome! This app is built with Next.js and Firebase.</p>
         <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
 
