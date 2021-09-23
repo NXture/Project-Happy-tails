e4b06547d133f91800cf04bc7a74011204cd4f83
NXture <varunvaru27101999@gmail.com>
2021-09-23 19:10:15 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,24 @@ import { UserContext } from "@lib/context";
 import { useUserData } from "@lib/hooks";
 import { Toaster } from "react-hot-toast";
 import { Header } from "@components/Navbar/Header";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
+  const router = useRouter();
 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.local.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <UserContext.Provider value={userData}>
       <Header />
