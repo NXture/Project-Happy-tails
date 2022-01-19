15b4e2312e7fe0892eb4b9ceb0458d57f31d8324
Varun <varunvaru27101999@gmail.com>
2022-01-19 23:41:34 +0530

    created manifest file for PWA

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <link rel="manifest" href="/app.webmanifest" />
         </Head>
         <body>
           <Main />
